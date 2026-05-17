db454f41b88c7e2f408da393dc0c9b6cdebb2bc7
A$APk <const.qqdae@gmail.com>
2026-05-18 02:39:13 +0600

    added playlists

---
+++ playlists.py
@@ -0,0 +1,11 @@
+sad_playlist = [
+    "Mac Miller — 2009",
+    "Tyler, The Creator — Garden Shed",
+    "Frank Ocean — Nights"
+]
+
+happy_playlist = [
+    "JAY-Z — 4:44",
+    "Lil Uzi Vert — 20 Min",
+    "Sade — Kiss of Life "
+]
